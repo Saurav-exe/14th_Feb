0463d068ab24e09d661f4208284ced86bfafbb5e
Saurav-exe <sauravnp0903@gmail.com>
2026-02-03 17:38:42 +0530

    initial Commit

---
+++ script.js
@@ -0,0 +1,39 @@
+const params = new URLSearchParams(window.location.search);
+const to = params.get("to");
+const from = params.get("from");
+
+if (to) {
+  document.getElementById("question").textContent =
+    `${to}, will you be my Valentine?`;
+}
+
+if (from) {
+  result.textContent = `— ${from}`;
+}
+
+const noBtn = document.getElementById("noBtn");
+const yesBtn = document.getElementById("yesBtn");
+const result = document.getElementById("result");
+
+const container = document.querySelector(".buttons");
+
+function moveNoButton() {
+  const containerRect = container.getBoundingClientRect();
+  const btnRect = noBtn.getBoundingClientRect();
+
+  const maxX = containerRect.width - btnRect.width;
+  const maxY = containerRect.height - btnRect.height;
+
+  const randomX = Math.random() * maxX;
+  const randomY = Math.random() * maxY;
+
+  noBtn.style.left = `${randomX}px`;
+  noBtn.style.top = `${randomY}px`;
+}
+
+noBtn.addEventListener("mouseover", moveNoButton);
+noBtn.addEventListener("touchstart", moveNoButton);
+
+yesBtn.addEventListener("click", () => {
+  result.textContent = "Yay! 💖 I knew it!";
+});
